5883a2e8f5102372ef190af522ddd84497952995
RayaneMeyfr <rayanemeyfr@hotmail.com>
2025-08-01 15:31:26 +0200

    Ajout de tout update, delete et findbyid

--- exo5/src/main/java/org/example/exo5/controller/VoitureResource.java
+++ exo5/src/main/java/org/example/exo5/controller/VoitureResource.java
@@ -31,14 +31,64 @@ public class VoitureResource {
         return this.voitureService.getVoitures();
     }
 
-    @GET
+    @POST
     @Path("/create")
     public Voiture createVoiture(
             @QueryParam("marque")String marque,
             @QueryParam("anneefabrication") int anneefabrication,
             @QueryParam("couleur")String couleur)
     {
-        voitureService.add(new Voiture(voitureService.getVoitures().size() + 1,marque,anneefabrication,couleur));
-        return voitureService.getById(voitureService.getVoitures().size());
+        int id = this.voitureService.getVoitures().size() + 1;
+        Voiture voiture = null;
+        if(this.voitureService.getById(id) != null) {
+            voiture = new Voiture(this.voitureService.getVoitures().size() + 2,marque,anneefabrication,couleur);
+        }else {
+            voiture = new Voiture(this.voitureService.getVoitures().size() + 1,marque,anneefabrication,couleur);
+        }
+        this.voitureService.add(voiture);
+        return voiture;
+    }
+
+    @PUT
+    @Path("/update")
+    public Voiture updateVoiture(@QueryParam("id") int id,
+                                 @QueryParam("marque")String marque,
+                                 @QueryParam("anneefabrication") String anneefabrication,
+                                 @QueryParam("couleur")String couleur
+    ){
+        Voiture voiture = this.voitureService.getById(id);
+        if(voiture == null){
+            return null;
+        }else{
+            if(marque != null){
+                voiture.setMarque(marque);
+            }
+            if(anneefabrication != null){
+                voiture.setAnneeFabrication(Integer.parseInt(anneefabrication));
+            }
+            if(couleur != null){
+                voiture.setCouleur(couleur);
+            }
+
+            this.voitureService.update(voiture);
+
+            return voiture;
+        }
+
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    public Voiture deleteVoiture(@PathParam("id") int id) {
+        Voiture voiture = this.voitureService.getById(id);
+        this.voitureService.delete(voiture);
+        return voiture;
+    }
+
+    @GET
+    @Path("/{id}")
+    public Voiture getById(@PathParam("id") int id) {
+        Voiture voiture = this.voitureService.getById(id);
+        return voiture;
     }
 }
